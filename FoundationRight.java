8402ec42b827c6a0901d4e80fe3cd7a16348b6ca
Steven Tan <50270633+StevenTan123@users.noreply.github.com>
2019-12-27 11:17:05 -0500

    Update FoundationRight.java

--- FoundationRight.java
+++ FoundationRight.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -63,32 +34,56 @@ public class FoundationRight extends LinearOpMode {
     private DcMotor frontRight = null;
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
-    DcMotor vert_motor = null;
-    Servo armservo = null;
-    double angle;
+    Servo leftClip = null;
+    Servo rightClip = null;
+    //DcMotor vert_motor = null;
+    //DcMotor horz_motor = null;
+    //Servo armservo = null;
+    double startangle, angle;
     double x, y, fieldwidth, fieldlength;
+    double skystoneColorThreshold, distanceBeforeBlocks, inchesStrafePerSec, degreesTurnPerSec, inchesAdvancePerSec, edgeToBlocks, robotWidth, robotLength;
     ColorSensor color_sensor;
     DistanceSensor dist_sensor;
     BNO055IMU imu;
-    DataLogger datalog;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        angle = 180;
-        x = 134;
-        y = 108;
+
+
+        //Constant initializations
+        skystoneColorThreshold = 1;
+        distanceBeforeBlocks = 5;
+        inchesStrafePerSec = 11;
+        degreesTurnPerSec = 80;
+        inchesAdvancePerSec = 14;
+        edgeToBlocks = 48;
+        robotLength = 18;
+        robotWidth = 16;
+
+
+        //Coordinate reference frame initialization
+        angle = 0;
+        startangle = angle;
         fieldwidth = 144;
         fieldlength = 144;
+        x = fieldwidth - robotLength / 2;
+        y = 48;
+
+        //Sensor and motor initializations
         frontLeft = hardwareMap.get(DcMotor.class, "front_left");
         frontRight = hardwareMap.get(DcMotor.class, "front_right");
         backLeft = hardwareMap.get(DcMotor.class, "back_left");
         backRight = hardwareMap.get(DcMotor.class, "back_right");
         color_sensor = hardwareMap.get(ColorSensor.class,"clr");
-        vert_motor = hardwareMap.get(DcMotor.class, "vert_motor");
-        armservo = hardwareMap.get(Servo.class, "arm_servo");
+        leftClip = hardwareMap.get(Servo.class, "left_clip");
+        rightClip = hardwareMap.get(Servo.class, "right_clip");
+        //vert_motor = hardwareMap.get(DcMotor.class, "vert_motor");
+        //horz_motor = hardwareMap.get(DcMotor.class, "horz_motor");
+        //armservo = hardwareMap.get(Servo.class, "arm_servo");
         dist_sensor = hardwareMap.get(DistanceSensor.class, "distance");
+
+
+        //Gyro initialization
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.mode = BNO055IMU.SensorMode.IMU;
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
@@ -106,14 +101,54 @@ public class FoundationRight extends LinearOpMode {
         telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
         telemetry.update();
 
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
         if(opModeIsActive()) {
-            moveTo(120, 108);
-            moveTo(120, 72);
+            leftClip.setPosition(1);
+            rightClip.setPosition(0);
+            sleep(1000);
+            advance(10);
+            x -= 10;
+            strafeAmount(20, 0);
+            double distance = dist_sensor.getDistance(DistanceUnit.INCH);
+            while(distance > 2) {
+                dirForward();
+                setPower(0.5);
+                distance = dist_sensor.getDistance(DistanceUnit.INCH);
+            }
+            setPower(0);
+            x = fieldwidth - 48 + robotLength / 2;
+            turn(0, 0);
+            leftClip.setPosition(0);
+            rightClip.setPosition(1);
+            sleep(1000);
+            backadvance(60);
+            x = fieldwidth - robotLength / 2;
+            leftClip.setPosition(1);
+            rightClip.setPosition(0);
+            sleep(1000);
+            strafeAmount(45, 1);
         }
     }
+
+    //Strafes inches in certain direction. 0 means right while 1 means left
+    public void strafeAmount(double inchesStrafe, int direction) {
+        if(direction == 1) {
+            dirStrafeLeft();
+            y -= inchesStrafe;
+        }else {
+            dirStrafeRight();
+            y += inchesStrafe;
+        }
+        setPower(0.5);
+        double timeWait = inchesStrafe / inchesStrafePerSec * 1000;
+        sleep((long)timeWait);
+        setPower(0);
+    }
+
+    //Move robot to (destx, desty)
     public void moveTo(double destx, double desty) {
         double xdif = destx - x;
         double ydif = desty - y;
@@ -128,34 +163,39 @@ public class FoundationRight extends LinearOpMode {
         }
         telemetry.addData("Degree of Rotation", "{%.2f}", angRot);
         telemetry.update();
-        turn(angRot);
+        turn(angRot, 0);
         advance(distance(x, y, destx, desty));
         x = destx;
         y = desty;
     }
-    public void turn(double rotToAng) {
+
+    //Turn to rotToAng
+    public void turn(double rotToAng, int level) {
+        if(level >= 2) {
+            return;
+        }
         setPower(0.5);
         double angDif = rotToAng - angle;
-        if(angDif < 0){
+        if (angDif < 0) {
             angDif += 360;
         }
-        if(angDif > 180) {
+        if (angDif > 180) {
             angDif -= 360;
             angDif *= -1;
             dirLeft();
-        }else {
+        } else {
             dirRight();
         }
         setPower(0.5);
-        double waitTime = angDif / 95 * 1000;
+        double waitTime = angDif / degreesTurnPerSec * 1000;
         angle = rotToAng;
         sleep((long)waitTime);
         setPower(0);
-        sleep(2000);
+        sleep(1000);
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         float actualAngle = angles.firstAngle;
         actualAngle = 360 - actualAngle;
-        actualAngle += 180;
+        actualAngle += startangle;
         if(actualAngle >= 360){
             actualAngle -= 360;
         }
@@ -164,13 +204,20 @@ public class FoundationRight extends LinearOpMode {
         if(correction < 5 || correction > 355) {
             return;
         }else{
-            turn(rotToAng);
+            turn(rotToAng, level + 1);
         }
     }
     public void advance(double distance) {
         dirForward();
         setPower(0.5);
-        double waitTime = distance / 13 * 1000;
+        double waitTime = distance / inchesAdvancePerSec * 1000;
+        sleep((long)waitTime);
+        setPower(0);
+    }
+    public void backadvance(double distance) {
+        dirBackward();
+        setPower(0.5);
+        double waitTime = distance / inchesAdvancePerSec * 1000;
         sleep((long)waitTime);
         setPower(0);
     }
@@ -198,11 +245,23 @@ public class FoundationRight extends LinearOpMode {
         backLeft.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.FORWARD);
     }
-    public void strafe() {
+    public void dirBackward() {
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+    public void dirStrafeRight() {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.FORWARD);
+    }
+    public void dirStrafeLeft() {
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
     }
     public double distance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
